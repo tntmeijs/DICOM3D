88ff854b9c9c0801182d348684c9c128a1035dbd
tntmeijs <tntmeijs@gmail.com>
2019-09-15 14:43:07 +0100

    Renderer initialization

---
+++ src/renderer/renderer.cpp
@@ -0,0 +1,50 @@
+#include "renderer.hpp"
+
+dcm::DCMRenderer::DCMRenderer() :
+	m_gl_major(0),
+	m_gl_minor(0),
+	m_width(0),
+	m_height(0)
+{
+}
+
+dcm::DCMRenderer::~DCMRenderer()
+{
+}
+
+void dcm::DCMRenderer::Initialize(int initial_width, int initial_height, int gl_major, int gl_minor)
+{
+	m_gl_major = gl_major;
+	m_gl_minor = gl_minor;
+	m_width = initial_width;
+	m_height = initial_height;
+
+	// Configure GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, m_gl_major);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, m_gl_minor);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Initialize GL3W
+	if (gl3wInit())
+	{
+		spdlog::critical("Failed to initialize GL3W.");
+		glfwTerminate();
+		return;
+	}
+
+	// OpenGL version not supported
+	if (!gl3wIsSupported(4, 6))
+	{
+		spdlog::critical("OpenGL %i.%i not supported.", m_gl_major, m_gl_minor);
+		glfwTerminate();
+		return;
+	}
+}
+
+void dcm::DCMRenderer::Render() const
+{
+}
+
+void dcm::DCMRenderer::CleanUp()
+{
+}
