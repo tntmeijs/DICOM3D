79b21ca1674e87a52c0eb947b101e768fefe929e
tntmeijs <tntmeijs@gmail.com>
2019-09-22 14:39:43 +0100

    Empty OpenGL shader class

---
+++ src/renderer/gl_shader.cpp
@@ -0,0 +1,1 @@
+#include "gl_shader.hpp"
---
+++ src/renderer/gl_shader.hpp
@@ -0,0 +1,20 @@
+#ifndef DICOM3D_GL_SHADER_HPP
+#define DICOM3D_GL_SHADER_HPP
+
+namespace dcm
+{
+	/**
+	 * An OpenGL shader
+	 */
+	class DCMGLShader
+	{
+	public:
+		DCMGLShader();
+		~DCMGLShader();
+
+	private:
+
+	};
+}
+
+#endif //! DICOM3D_GL_SHADER_HPP
