9a88a39d202d4a176ced1c379c4c8310b9002671
tntmeijs <tntmeijs@gmail.com>
2019-10-12 18:08:43 +0100

    Compute shader renaming
    [RENAME] Changes variable names so they are consistent with the code style.

--- resources/shaders/volumetric.cs
+++ resources/shaders/volumetric.cs
@@ -1,20 +1,20 @@
 #version 460
 
-layout(rgba32f, binding = 0) uniform image2D outputTex;
-layout(binding = 0) uniform sampler3D volumeTex;
+layout(rgba32f, binding = 0) uniform image2D output_texture;
+layout(binding = 0) uniform sampler3D volume_data;
 
 layout(local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
 
 void main()
 {
     // Pixel position in texture pixels
-    ivec2 pixelPos = ivec2(gl_GlobalInvocationID.xy);
+    ivec2 pixel_position = ivec2(gl_GlobalInvocationID.xy);
 
     // Size of the output texture in pixels
-    vec2 imageSize = imageSize(outputTex);
+    vec2 image_size = image_size(output_texture);
 
     // Normalized screen-space texture coordinates (0 to 1)
-    vec2 uv = pixelPos / imageSize;
+    vec2 uv = pixel_position / image_size;
 
-    imageStore(outputTex, pixelPos, vec4(1.0, uv, 1.0));
+    imageStore(output_texture, pixel_position, vec4(1.0, uv, 1.0));
 }
